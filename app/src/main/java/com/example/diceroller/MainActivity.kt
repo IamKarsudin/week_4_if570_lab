ebe714afcbd0ef57173774296483b48fef4d7009
IamKarsudin <karsudin@student.umn.ac.id>
2023-05-06 10:37:28 +0700

    Clean Code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,13 +12,22 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
     }
 }
 
-class Dice(val numSides: Int) {
+class Dice(private val numSides: Int) {
 
     fun roll(): Int {
         return (1..numSides).random()
